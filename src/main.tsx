d3106057ccdaa4daaf40e363cfa2fdfd6346aead
Daniel Thornton <d.r.thornton@hotmail.com>
2024-03-09 16:44:16 +1100

    fix: amplify styling added
    added react router dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/app";
 import "./index.css";
-
+import "@aws-amplify/ui-react/styles.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   <React.StrictMode>
+    <App />
     <App />
   </React.StrictMode>
 );
