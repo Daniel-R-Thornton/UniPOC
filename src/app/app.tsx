8532c6060ac165178542cdc8b5dd4ed44de63307
Daniel Thornton <d.r.thornton@hotmail.com>
2024-03-09 16:16:59 +1100

    feat: added aside, added blurb, added responsive layout
    removed aside from blurb moved to app for now, will realistically go away anyway!

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,14 +5,31 @@ function App() {
   return (
     <main>
       <Header logo="Logo" loginButton="Login" />
-      <Blurb
-        aside="Aside"
-        body={{
-          text: "Some text content here...",
-          subtitle: "Some subtext content here...",
-        }}
-        image="https://via.placeholder.com/150"
-      />
+      <div className="grid h-full w-full grid-cols-1 md:grid-cols-2 items-center">
+        <div className="col-span-1">
+          <Blurb
+            body={{
+              text: "Are you ready to become an Idea Champion? Share your innovative ideas or build upon the ideas of others! This portal will remain open for the next year and we will refer to it throughout the engagement process. Participants whose ideas best address the challenges that our community faces will be invited to partner with us to further develop their ideas! We encourage one idea per card and building off the ideas of others. Only use the location feature if your idea is specific to a place. Remember, you can submit as many ideas as you want!",
+              subtitle: "Ideas Portal ",
+            }}
+            image="https://stlouis.citizenlab.co/uploads/e0d13d54-d706-4d53-af85-2f6c48c4c2fe/project/header_bg/1c7aa1cb-7c75-4293-8f9b-37fa72a73657/large_afd08489-8a69-4e30-9591-974d82716d46.jpeg"
+          />
+        </div>
+        <div className="col-span-1">
+          <div className="flex flex-col gap-4 p-4 ">
+            <div className="bg-gray-200 p-4">
+              <h2 className="text-xl">About Us</h2>
+              <p className="text-sm pt-1">
+                lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Quisnostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat. Duis aute irure dolor in reprehenderit in
+                voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- src/components/blurb/blurb.tsx
+++ src/components/blurb/blurb.tsx
@@ -2,25 +2,28 @@ import React from "react";
 
 interface BlurbProps {
   image: string;
+  imageAlt?: string;
   body: {
-    text: string;
+    text: React.ReactNode;
     subtitle: string;
   };
-  aside: React.ReactNode;
 }
 
-const Blurb: React.FC<BlurbProps> = ({ image, body, aside }) => {
+const Blurb: React.FC<BlurbProps> = ({ image, body, imageAlt }) => {
   return (
-    <article className="flex">
-      <figure className="w-1/2">
-        <img src={image} alt="Blurb Image" className="w-full" />
-        <figcaption>Image description</figcaption>
+    <article className="flex flex-col px-10 m-10">
+      <figure className="w-full">
+        <img
+          src={image}
+          alt={imageAlt}
+          className="w-full h-30 object-fill aspect-[4/1]"
+        />
+        <figcaption className="hidden">{imageAlt}</figcaption>
       </figure>
-      <section className="w-1/2 p-4">
+      <section className="p-4 flex flex-col gap-2 ">
         <h2 className="text-lg">{body.subtitle}</h2>
-        <p>Some text content here...</p>
+        <p className="">{body.text}</p>
       </section>
-      <aside className="w-1/4 p-4">{aside}</aside>
     </article>
   );
 };
